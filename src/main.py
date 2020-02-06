18a14e250f43a1e54ec052f5bd94d33eb6e0fdaa
wollemat <f.c.slothouber@gmail.com>
2020-02-06 12:37:45 +0100

    Implement full transit modeling

--- src/main.py
+++ src/main.py
@@ -9,12 +9,15 @@ STAR_LIMB_DARKENING_COEFFICIENT = 0.80
 STAR_LIMB_DARKENING_ALPHA = 0.85
 PLANET_RADIUS = 500
 KM_PER_PIXEL = 10
+PLANET_INCLINATION = 0.2
+SAMPLE_RATE = 0.05
 
 # private variables
 
 _star_pixel_radius = int(STAR_RADIUS / KM_PER_PIXEL)
 _planet_pixel_radius = int(PLANET_RADIUS / KM_PER_PIXEL)
 _grid_size = int(2 * _star_pixel_radius + 1)
+_planet_y = _grid_size * PLANET_INCLINATION
 
 
 def calc_star_brightness(d_star_center):
@@ -23,7 +26,7 @@ def calc_star_brightness(d_star_center):
     return 255 * limb
 
 
-def generate_snapshot_image(planet_x, planet_y):
+def generate_snapshot_image(planet_x):
     grid = numpy.zeros((_grid_size, _grid_size))
 
     for x in range(_grid_size):
@@ -36,7 +39,7 @@ def generate_snapshot_image(planet_x, planet_y):
                 continue
 
             dx_planet_center = abs(planet_x - x)
-            dy_planet_center = abs(planet_y - y)
+            dy_planet_center = abs(_planet_y - y)
             d_planet_center = math.sqrt(dx_planet_center ** 2 + dy_planet_center ** 2)
 
             if d_planet_center < _planet_pixel_radius:
@@ -48,6 +51,12 @@ def generate_snapshot_image(planet_x, planet_y):
 
 
 if __name__ == '__main__':
-    star = generate_snapshot_image(300, 700)
-    plt.imshow(star.T, origin='lower')
+    samples = int(_grid_size * SAMPLE_RATE)
+    transit = numpy.zeros(samples)
+
+    for t in range(samples):
+        transit[t] = generate_snapshot_image(int(t * _grid_size / samples)).mean()
+        print("Finished %d of %d" % (t + 1, samples))
+
+    plt.plot(transit)
     plt.show()
