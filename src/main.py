71024360528a1b1d93954abe9c4f7bcfb8b006a4
wollemat <f.c.slothouber@gmail.com>
2020-02-06 01:14:24 +0100

    Implement limb darkening effect

--- src/main.py
+++ src/main.py
@@ -6,6 +6,8 @@ import numpy as numpy
 # Configuration
 
 STAR_PIXEL_RADIUS = 256
+STAR_LIMB_DARKENING_COEFFICIENT = 0.80
+STAR_LIMB_DARKENING_ALPHA = 0.85
 
 # private variables
 
@@ -19,10 +21,12 @@ def generate_star():
         for y in range(_grid_height):
             dx_star_center = STAR_PIXEL_RADIUS - x
             dy_star_center = STAR_PIXEL_RADIUS - y
-            pixel_distance_star_center = math.sqrt(dx_star_center ** 2 + dy_star_center ** 2)
+            pixel_distance_star_center = math.sqrt(dx_star_center**2 + dy_star_center**2)
 
             if pixel_distance_star_center <= STAR_PIXEL_RADIUS:
-                grid[x, y] = 255
+                tmp = (1 - ((pixel_distance_star_center / STAR_PIXEL_RADIUS)**2))**0.5
+                limb = 1 - STAR_LIMB_DARKENING_COEFFICIENT * (1 - (tmp**STAR_LIMB_DARKENING_ALPHA))
+                grid[x, y] = 255 * limb
     return grid
 
 
