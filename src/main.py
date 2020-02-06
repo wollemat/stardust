02813e8dd013938001eaa638f67d659246b1045b
wollemat <f.c.slothouber@gmail.com>
2020-02-06 11:40:16 +0100

    Implement initial atmosphere

--- src/main.py
+++ src/main.py
@@ -16,40 +16,47 @@ KM_PER_PIXEL = 10
 
 _star_pixel_radius = int(STAR_RADIUS / KM_PER_PIXEL)
 _planet_pixel_radius = int(PLANET_RADIUS / KM_PER_PIXEL)
+_atm_pixel_radius = int(60)
 _grid_size = int(2 * _star_pixel_radius + 1)
 
 
-def generate_star():
+def calc_star_brightness(d_star_center):
+    tmp = (1 - ((d_star_center / _star_pixel_radius) ** 2)) ** 0.5
+    limb = 1 - STAR_LIMB_DARKENING_COEFFICIENT * (1 - (tmp ** STAR_LIMB_DARKENING_ALPHA))
+    return 255 * limb
+
+
+def generate_snapshot_image(planet_x, planet_y):
     grid = numpy.zeros((_grid_size, _grid_size))
+
     for x in range(_grid_size):
         for y in range(_grid_size):
             dx_star_center = _star_pixel_radius - x
             dy_star_center = _star_pixel_radius - y
-            pixel_distance_star_center = math.sqrt(dx_star_center ** 2 + dy_star_center ** 2)
+            d_star_center = math.sqrt(dx_star_center ** 2 + dy_star_center ** 2)
 
-            if pixel_distance_star_center <= _star_pixel_radius:
-                tmp = (1 - ((pixel_distance_star_center / _star_pixel_radius) ** 2)) ** 0.5
-                limb = 1 - STAR_LIMB_DARKENING_COEFFICIENT * (1 - (tmp ** STAR_LIMB_DARKENING_ALPHA))
-                grid[x, y] = 255 * limb
+            if d_star_center > _star_pixel_radius:
+                continue
+
+            dx_planet_center = abs(planet_x - x)
+            dy_planet_center = abs(planet_y - y)
+            d_planet_center = math.sqrt(dx_planet_center ** 2 + dy_planet_center ** 2)
+
+            if d_planet_center < _planet_pixel_radius:
+                continue
+
+            star_brightness = calc_star_brightness(d_star_center)
+
+            if d_planet_center < _atm_pixel_radius:
+                grid[x, y] = 100  # TODO implement atmosphere
+                continue
+
+            grid[x, y] = star_brightness
+
     return grid
 
 
-def cut_out_planet(original_star, planet_x, planet_y):
-    grid_copy = numpy.copy(original_star)
-
-    for x in range(max(0, planet_x - _planet_pixel_radius), min(planet_x + _planet_pixel_radius, _grid_size)):
-        for y in range(max(0, planet_y - _planet_pixel_radius), min(planet_y + _planet_pixel_radius, _grid_size)):
-            dx = abs(planet_x - x)
-            dy = abs(planet_y - y)
-            dp = math.sqrt(dx ** 2 + dy ** 2)
-
-            if dp < _planet_pixel_radius:
-                grid_copy[x, y] = 0
-
-    return grid_copy
-
-
 if __name__ == '__main__':
-    star = cut_out_planet(generate_star(), 300, 700)
+    star = generate_snapshot_image(300, 700)
     plt.imshow(star.T, origin='lower')
     plt.show()
