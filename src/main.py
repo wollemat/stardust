64adaac52d777646ae2d3d113e8975572aeb1e76
wollemat <f.c.slothouber@gmail.com>
2020-02-06 20:44:49 +0100

    Implement transit video generation

--- src/main.py
+++ src/main.py
@@ -1,66 +1,133 @@
-import math as math
-import matplotlib.pyplot as plt
+import math
 import numpy as np
 
-from multiprocessing import Pool
+from cv2 import VideoWriter
+from cv2 import VideoWriter_fourcc
+
+from multiprocessing import Process
+from multiprocessing import Pipe
 
 # Configuration
 
-STAR_RADIUS = 5000
+STAR_RADIUS = 462
 STAR_LIMB_DARKENING_COEFFICIENT = 0.80
 STAR_LIMB_DARKENING_ALPHA = 0.85
-PLANET_RADIUS = 500
-KM_PER_PIXEL = 10
+
+PLANET_RADIUS = 50
 PLANET_INCLINATION = 0.2
-SAMPLE_RATE = 0.05
-NUMBER_OF_WORKERS = 12
 
-# private variables
+MARGIN = 50
+NUMBER_OF_WORKERS = 16
 
-_star_pixel_radius = int(STAR_RADIUS / KM_PER_PIXEL)
-_planet_pixel_radius = int(PLANET_RADIUS / KM_PER_PIXEL)
-_grid_size = int(2 * _star_pixel_radius + 1)
+# Private variables
+
+_grid_size = int(2 * (STAR_RADIUS + MARGIN))
 _planet_y = _grid_size * PLANET_INCLINATION
 
 
 def calc_star_brightness(d_star_center):
-    tmp = (1 - ((d_star_center / _star_pixel_radius) ** 2)) ** 0.5
-    limb = 1 - STAR_LIMB_DARKENING_COEFFICIENT * (1 - (tmp ** STAR_LIMB_DARKENING_ALPHA))
-    return 255 * limb
+    tmp = (1 - ((d_star_center / STAR_RADIUS) ** 2)) ** 0.5
+    return 1 - STAR_LIMB_DARKENING_COEFFICIENT * (1 - (tmp ** STAR_LIMB_DARKENING_ALPHA))
 
 
 def generate_snapshot_image(planet_x):
-    grid = np.zeros((_grid_size, _grid_size))
-
+    grid = np.zeros((_grid_size, _grid_size, 3), dtype=np.uint8)
     for x in range(_grid_size):
         for y in range(_grid_size):
-            dx_star_center = _star_pixel_radius - x
-            dy_star_center = _star_pixel_radius - y
+            dx_star_center = STAR_RADIUS - x
+            dy_star_center = STAR_RADIUS - y
             d_star_center = math.sqrt(dx_star_center ** 2 + dy_star_center ** 2)
 
-            if d_star_center > _star_pixel_radius:
+            if d_star_center > STAR_RADIUS:
                 continue
 
             dx_planet_center = abs(planet_x - x)
             dy_planet_center = abs(_planet_y - y)
             d_planet_center = math.sqrt(dx_planet_center ** 2 + dy_planet_center ** 2)
 
-            if d_planet_center < _planet_pixel_radius:
+            if d_planet_center < PLANET_RADIUS:
                 continue
 
-            grid[x, y] = calc_star_brightness(d_star_center)
+            brightness = calc_star_brightness(d_star_center)
+            grid[x, y, 0] = 255 * brightness
+            grid[x, y, 1] = 165 * brightness
 
     return grid
 
 
-def worker_function(x):
-    snapshot = generate_snapshot_image(x)
-    return snapshot.mean()
+def worker_function(conn):
+    msg = conn.recv()
+
+    if isinstance(msg, int):
+        print("Modeling snapshot %d" % msg)
+        conn.send(generate_snapshot_image(msg))
+        worker_function(conn)
+
+    if isinstance(msg, str):
+        if msg == "stop":
+            print("Stopping")
+            exit()
+
+    print("Something went wrong")
+
+
+def initialize_workers(conns, processes):
+    for i in range(NUMBER_OF_WORKERS):
+        parent_connection, child_connection = Pipe()
+        process = Process(target=worker_function, args=(child_connection,))
+        processes.append(process)
+        conns.append(parent_connection)
+    print("Workers are initialized")
+
+
+def start_workers(processes):
+    for i in range(NUMBER_OF_WORKERS):
+        processes[i].start()
+    print("Workers have started")
+
+
+def delegate_workers(conns, counter):
+    start_counter = counter
+    for i in range(NUMBER_OF_WORKERS):
+        if counter == _grid_size:
+            break
+        conns[i].send(counter)
+        counter += 1
+    print("Frames %d until %d have been delegated" % (start_counter, counter - 1))
+
+
+def collect_from_workers(conns, counter, video):
+    start_counter = counter
+    for i in range(NUMBER_OF_WORKERS):
+        if counter == _grid_size:
+            break
+        video.write(conns[i].recv())
+        counter += 1
+    print("Frames %d until %d have been collected" % (start_counter, counter - 1))
+
+
+def stop_workers(conns, processes):
+    for i in range(NUMBER_OF_WORKERS):
+        conns[i].send("stop")
+    for i in range(NUMBER_OF_WORKERS):
+        processes[i].join()
+    print("Workers have been stopped")
 
 
 if __name__ == '__main__':
-    with Pool(NUMBER_OF_WORKERS) as pool:
-        transit = pool.map(worker_function, np.arange(0, _grid_size, _grid_size * SAMPLE_RATE, dtype=int))
+    video = VideoWriter('./data/transit.mkv', VideoWriter_fourcc(*'X264'), 24.0, (_grid_size, _grid_size))
+    counter = 0
+    processes = []
+    conns = []
 
-    plt.plot(transit)
-    plt.show()
+    initialize_workers(conns, processes)
+    start_workers(processes)
+
+    while counter < _grid_size:
+        delegate_workers(conns, counter)
+        collect_from_workers(conns, counter, video)
+        counter += NUMBER_OF_WORKERS
+
+    stop_workers(conns, processes)
+
+    video.release()
