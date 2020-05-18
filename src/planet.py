be674db80b7af86ad54fd247bf86e9d138c68154
wollemat <f.c.slothouber@gmail.com>
2020-05-18 21:32:59 +0200

    Remove multiple planet functionality

--- src/planet.py
+++ src/planet.py
@@ -4,29 +4,15 @@ import numpy as np
 
 from star import generate_star
 
-from config import PLANET_1_RADIUS
-from config import PLANET_1_INCLINATION
-
-from config import PLANET_2
-from config import PLANET_2_RADIUS
-from config import PLANET_2_INCLINATION
-from config import PLANET_2_SPEED_FACTOR
-from config import PLANET_2_OFFSET
-
-from config import PLANET_3
-from config import PLANET_3_RADIUS
-from config import PLANET_3_INCLINATION
-from config import PLANET_3_SPEED_FACTOR
-from config import PLANET_3_OFFSET
+from config import PLANET_RADIUS
+from config import PLANET_INCLINATION
 
 from config import STAR_RADIUS
 from config import MARGIN
 from config import _GRID_SIZE
 
 _STAR = generate_star()  # Cached star image.
-_PLANET_1_Y = int(STAR_RADIUS + MARGIN + PLANET_1_INCLINATION)  # Y coordinate of planet 1.
-_PLANET_2_Y = int(STAR_RADIUS + MARGIN + PLANET_2_INCLINATION)  # Y coordinate of planet 2.
-_PLANET_3_Y = int(STAR_RADIUS + MARGIN + PLANET_3_INCLINATION)  # Y coordinate of planet 3.
+_PLANET_Y = int(STAR_RADIUS + MARGIN + PLANET_INCLINATION)  # Y coordinate of planet.
 
 
 #
@@ -51,18 +37,12 @@ def cut_out_planet(grid, planet_x, planet_y, planet_radius):
 
 
 #
-# This function makes a copy of the cached star image and cuts out the 3 planets. Afterwards it flips
+# This function makes a copy of the cached star image and cuts out the planet. Afterwards it flips
 # and transposes the image such that it is rendered correctly.
 #
-# x: The x coordinate of the center of planet 1.
+# x: The x coordinate of the center of the planet.
 #
 def cut_out_planets(x):
     grid = _STAR.copy()
-
-    cut_out_planet(grid, x, _PLANET_1_Y, PLANET_1_RADIUS)
-    if PLANET_2:
-        cut_out_planet(grid, int(x * PLANET_2_SPEED_FACTOR + PLANET_2_OFFSET), _PLANET_2_Y, PLANET_2_RADIUS)
-    if PLANET_3:
-        cut_out_planet(grid, int(x * PLANET_3_SPEED_FACTOR + PLANET_3_OFFSET), _PLANET_3_Y, PLANET_3_RADIUS)
-
+    cut_out_planet(grid, x, _PLANET_Y, PLANET_RADIUS)
     return np.flip(np.swapaxes(grid, 0, 1), 0)
