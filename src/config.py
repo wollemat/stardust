9a3115445a128d45421817b304da61ec05820ad2
wollemat <f.c.slothouber@thewoll.com>
2020-09-30 16:23:15 +0200

    Set default configuration

--- src/config.py
+++ src/config.py
@@ -2,13 +2,13 @@
 STAR_LIMB_DARKENING_COEFFICIENT = 0.80  # Coefficient for the limb darkening implementation (Sun = 0.80).
 STAR_LIMB_DARKENING_ALPHA = 0.85  # Alpha for the limb darkening implementation (Sun = 0.85).
 
-STAR_CHANNEL_RED = 189  # Red color channel value star.
-STAR_CHANNEL_GREEN = 40  # Green color channel value star.
-STAR_CHANNEL_BLUE = 28  # Blue color channel value star.
+STAR_CHANNEL_RED = 255  # Red color channel value star.
+STAR_CHANNEL_GREEN = 165  # Green color channel value star.
+STAR_CHANNEL_BLUE = 0  # Blue color channel value star.
 
-PLANET_SIZE = 100  # Size of planet diameter in pixels.
-PLANET_INIT = 250  # Start height of planet in pixels from the bottom.
-PLANET_ANGLE = 0  # Relative speed of planet orbit in y direction. Positive is up, negative is down.
+PLANET_SIZE = 80  # Size of planet diameter in pixels.
+PLANET_INIT = 150  # Start height of planet in pixels from the bottom.
+PLANET_ANGLE = 0.5  # Relative speed of planet orbit in y direction. Positive is up, negative is down.
 
 IMAGE_SIZE = 1024  # Image width and Image height.
 IMAGE_MARGIN = 50  # Image margin around star.
